750940bf94672d28eabeca3156aa13f9d8012cc1
TS-2210 <148909343+TS-2210@users.noreply.github.com>
2025-11-08 22:09:04 +0000

    fixed errors

--- map.js
+++ map.js
@@ -16,9 +16,10 @@ const lat = position.coords.latitude;
 const lon = position.coords.longitude;
 map.setView([lat, lon], 15);
 
-        // Add marker for user's location
-        const userMarker = L.marker([lat, lon]).addTo(map)
-        .bindPopup(`<b class="here">You are here</b>`).openPopup();
+L.marker([lat, lon])
+    .addTo(map)
+    .bindPopup("<b>You are here</b>")
+    .openPopup();
 
 const overpassUrl = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:2000,${lat},${lon})["shop"="supermarket"];out;`;
 
@@ -28,20 +29,6 @@ fetch(overpassUrl)
     data.elements.forEach(el => {
         if (el.lat && el.lon) {
         const name = el.tags.name || "Unnamed supermarket";
-                const website = el.tags.website || el.tags.url;
-
-                // Popup text: name + website link if available
-                let popupText = `<b class="popup-Text">${name}</b>`;
-                if (website) {
-                  popupText += `<br><a href="${website}" target="_blank">Visit website</a>`;
-                  if (name == "Co-op Food"){
-                    popupText += `<br><a href="https://shop.coop.co.uk/fruit-and-veg-deals" target="_blank"> <img class="deal" src = "https://images.ctfassets.net/5hwhsiyaz6z8/1onCTW7h4io9GQYXs18UdX/e579e095e1852fb9a9b66062df189d40/Fresh_3_-_2046_x_1150_px.png"><a>`
-                  }
-                  else if (name == "Morrisons"){
-                    popupText += `<br><a href="https://groceries.morrisons.com/promotions?srsltid=AfmBOorSaj4ZoAHam0g3T5jcT_xbq936Z56CWJLkLuOPSl6VxnfHiquQ" target="_blank"> <img class="deal" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwdwtcrN-cXY-P_GqfKXV6n3Gb1bvXi-3zxw&s"></a>`
-                  }
-                }
-
         const website = el.tags.website || el.tags.url;
 
         // Popup text: name + website link if available
