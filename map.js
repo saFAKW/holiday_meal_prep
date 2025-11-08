d23338b6c0b6ad2b952686ad5fee20f1bc6deb0f
Alif Nafili <alifnafili@gmail.com>
2025-11-08 21:28:08 +0000

    supermarket deals

--- map.js
+++ map.js
@@ -37,9 +37,15 @@
                 const website = el.tags.website || el.tags.url;
 
                 // Popup text: name + website link if available
-                let popupText = `<b>${name}</b>`;
+                let popupText = `<b class="popup-Text">${name}</b>`;
                 if (website) {
                   popupText += `<br><a href="${website}" target="_blank">Visit website</a>`;
+                  if (name == "Co-op Food"){
+                    popupText += `<br><a href="https://shop.coop.co.uk/fruit-and-veg-deals" target="_blank"> <img class="deal" src = "https://images.ctfassets.net/5hwhsiyaz6z8/1onCTW7h4io9GQYXs18UdX/e579e095e1852fb9a9b66062df189d40/Fresh_3_-_2046_x_1150_px.png"><a>`
+                  }
+                  else if (name == "Morrisons"){
+                    popupText += `<br><a href="https://groceries.morrisons.com/promotions?srsltid=AfmBOorSaj4ZoAHam0g3T5jcT_xbq936Z56CWJLkLuOPSl6VxnfHiquQ" target="_blank"> <img class="deal" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwdwtcrN-cXY-P_GqfKXV6n3Gb1bvXi-3zxw&s"></a>`
+                  }
                 }
 
                 L.marker([el.lat, el.lon])
