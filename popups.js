83f56f290411b81f6885aab7a1b45628c1f5d3f2
saFAKW <safakw13@gmail.com>
2025-11-08 17:06:59 +0000

    final push to solo branch

--- popups.js
+++ popups.js
@@ -1,1 +1,26 @@
-//insert code to monitor toggling pop ups (ingredient list, recipe list, supermarket lists)
+//event listeners
+// Close sheet when clicking outside
+document.addEventListener('click', function(event) {
+    const sheet = document.getElementById('ingredientsSheet');
+    const toggleBtn = document.querySelector('.toggle-btn');
+            
+    if (sheet.classList.contains('open') && 
+        !sheet.contains(event.target) && 
+        !toggleBtn.contains(event.target)) {                
+        sheet.classList.remove('open');
+        }
+        });
+
+//written functions
+function toggleSheet() {
+        const sheet = document.getElementById('ingredientsSheet');
+    sheet.classList.toggle('open');
+    }
+
+function makeMap(){
+    var location = {lat: 25.0330, lng: 121.5654};
+    var map  = new google.map.Map(document.getElementById("map-container"){
+        zoom: 4, 
+        center: location
+    });
+}
