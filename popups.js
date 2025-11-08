561a77bc3475bf4b19747701fa71339b1a25a77f
saFAKW <safakw13@gmail.com>
2025-11-08 17:21:01 +0000

    merge without conflicts 2

--- popups.js
+++ popups.js
@@ -19,7 +19,6 @@ function toggleSheet() {
 
 function makeMap(){
     var location = {lat: 25.0330, lng: 121.5654};
-    var map  = new google.map.Map(document.getElementById("map-container"){
-        zoom: 4, center: location
-    });
+    var map  = new google.map.Map(document.getElementById("map-container"),
+    {zoom: 4, center: location});
 }
