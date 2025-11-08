b60aa96eb31a674cc00447d5b95d9993e78cf2e5
saFAKW <safakw13@gmail.com>
2025-11-08 18:22:14 +0000

    temp

--- popups.js
+++ popups.js
@@ -15,10 +15,55 @@ document.addEventListener('click', function(event) {
 function toggleSheet() {
         const sheet = document.getElementById('ingredientsSheet');
     sheet.classList.toggle('open');
-    }
-
-function makeMap(){
-    var location = {lat: 25.0330, lng: 121.5654};
-    var map  = new google.map.Map(document.getElementById("map-container"),
-    {zoom: 4, center: location});
 }
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.866, lng: 151.196 },
+    zoom: 15,
+  });
+  const request = {
+    placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
+    fields: ["name", "formatted_address", "place_id", "geometry"],
+  };
+  const infowindow = new google.maps.InfoWindow();
+  const service = new google.maps.places.PlacesService(map);
+
+  service.getDetails(request, (place, status) => {
+    if (
+      status === google.maps.places.PlacesServiceStatus.OK &&
+      place &&
+      place.geometry &&
+      place.geometry.location
+    ) {
+      const marker = new google.maps.Marker({
+        map,
+        position: place.geometry.location,
+        title: place.name,
+      });
+
+      google.maps.event.addListener(marker, "click", () => {
+        const content = document.createElement("div");
+        const nameElement = document.createElement("h2");
+
+        nameElement.textContent = place.name;
+        content.appendChild(nameElement);
+
+        const placeIdElement = document.createElement("p");
+
+        placeIdElement.textContent = place.place_id;
+        content.appendChild(placeIdElement);
+
+        const placeAddressElement = document.createElement("p");
+
+        placeAddressElement.textContent = place.formatted_address;
+        content.appendChild(placeAddressElement);
+        infowindow.setContent(content);
+        infowindow.setOptions({ariaLabel: place.name});
+        infowindow.open(map, marker);
+      });
+    }
+  });
+}
+
+window.initMap = initMap;
