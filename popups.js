96efe7dce585a95a79f94439d92573b3b76b4a8c
saFAKW <safakw13@gmail.com>
2025-11-09 10:35:00 +0000

    added dropdown utensils HTML

--- popups.js
+++ popups.js
@@ -32,10 +32,22 @@ image.onmouseout = function(){
 }
 
 check = document.getElementById("portions").addEventListener("keydown", accessData)
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-function accessData(){
-    if (check){
-
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
-
+  }
 }
